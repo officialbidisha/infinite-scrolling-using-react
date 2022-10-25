ef465e80e094d52d59f78f0687429d5acf860526
Bidisha Das <officialbidisha1@gmail.com>
2022-10-25 10:55:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState, useRef, useCallback } from "react";
+import UserCard from "./components/UserCard";
+const TOTAL_PAGES = 3;
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  console.log(users.length);
+  
+  const observer = useRef(null);
+  const lastElementRef = useCallback((node)=> {
+    if(loading) return;
+    if(observer.current){
+      observer.current.disconnect();
+    }
+    observer.current = new IntersectionObserver((entries) => {
+      const first = entries[0];
+      if (first.isIntersecting) {
+        setPageNum((number) => number + 1);
+      }
+    });
+    if(node){
+      observer.current.observe(node);
+    }
+  
+  }, [loading, pageNum])
+
+  const callUser = async () => {
+    debugger;
+    setLoading(true);
+    let result = await fetch(
+      `https://randomuser.me/api/?page=${pageNum}&results=25&seed=abc`
+    );
+    let res = await result.json();
+
+    setUsers((prevResult) => [...prevResult, ...res.results]);
+    
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (pageNum <= TOTAL_PAGES) {
+      callUser();
+    }
+  }, [pageNum]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App row">
+      {users.map((user,index)=> {
+        if(users.length === index+1 && !loading && pageNum<=TOTAL_PAGES){
+          return  <div ref={lastElementRef}  className="r">  <UserCard data={user} key={index} ></UserCard></div>
+        }
+        else{
+          return <UserCard data={user} key={index}></UserCard>
+        }
+      })}
     </div>
   );
 }
